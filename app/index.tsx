f9791238057ecbe203e9e91deb813fa481418c40
Anirudha <anirudha.mahale@teknorix.com>
2025-03-26 13:03:54 +0530

    used the themes

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
 import { Redirect } from 'expo-router'
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 
 const showTabs = true;
 
@@ -15,9 +17,13 @@ const Index = () => {
 
     // If you want to see Index as first page.
     return (
-        <SafeAreaView>
-            <Text>Index</Text>
-        </SafeAreaView>
+        <ThemedView style={{gap: 16}}>
+            <ThemedText type='default'>This is default</ThemedText>
+            <ThemedText type='title'>This is title</ThemedText>
+            <ThemedText type='subtitle'>This is subtitle</ThemedText>
+            <ThemedText type='defaultSemiBold'>This is defaultSemiBold</ThemedText>
+            <ThemedText type='link'>This is link</ThemedText>
+        </ThemedView>
     )
 }
 
